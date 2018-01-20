67d1a5f77df2529b8cd269e4270976bae918b497
Thomas Helmrich <thomas@gigabit.de>
2018-01-20 19:56:29 +0100

    updated le_active checkbox

--- data/web/edit.php
+++ data/web/edit.php
@@ -215,7 +215,7 @@ if (isset($_SESSION['mailcow_cc_role'])) {
             <div class="form-group">
               <div class="col-sm-offset-2 col-sm-10">
                   <div class="checkbox">
-                      <label><input type="checkbox" value="1" name="active" <?=(isset($result['le_active_int']) && $result['le_active_int']=="1") ? "checked" : null;?> <?=($_SESSION['mailcow_cc_role'] == "admin") ? null : "disabled";?>> Let´s encrypt Zertifikat erstellen</label>
+                      <label><input type="checkbox" value="1" name="le_active" <?=(isset($result['le_active_int']) && $result['le_active_int']=="1") ? "checked" : null;?> <?=($_SESSION['mailcow_cc_role'] == "admin") ? null : "disabled";?>> Let´s encrypt Zertifikat erstellen</label>
                   </div>
               </div>
             </div>
